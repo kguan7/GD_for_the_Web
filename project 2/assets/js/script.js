26612e1d2e4cbc2e2c30e62d1831034559895057
Karen Guan <KarenGuan@karens-mbp.risd.edu>
2018-04-20 02:31:32 -0400

    yo

--- project 2/assets/js/script.js
+++ project 2/assets/js/script.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
 	// your code here
 
 
-$("#drake").click(function(){
+$("#wrapper").click(function(){
 	$("#jcole").toggle();
 });
+
+
 
 $("#jcole").click(function(){
 	$("#bigsean").toggle();
@@ -31,33 +33,303 @@ $("#acedro").click(function(){
 });
 
 $("#rocko").click(function(){
-	$("#webbie").toggle();
+	$(".spacer").toggle();
 });
 
 $("#webbie").click(function(){
 	$("#foxx").toggle();
 });
 
-$("#cardi").click(function(){
-	$("#nicki").toggle();
+$("#foxx").click(function(){
+	$("#bg").toggle();
 });
 
-$("#nicki").click(function(){
-	$("#tori").toggle();
+$("#bg").click(function(){
+	$("#mannie").toggle();
 });
 
-$("#tori").click(function(){
-	$("#meghan").toggle();
+$("#mannie").click(function(){
+	$("#bigtymers").toggle();
 });
 
-$("#meghan").click(function(){
-	$("#fifth").toggle();
+$("#bigtymers").click(function(){
+	$("#trick").toggle();
 });
 
-$("#fifth").click(function(){
-	$("#pretty").toggle();
+$("#trick").click(function(){
+	$("#pastor").toggle();
 });
 
+$("#pastor").click(function(){
+	$("#lilscrappy").toggle();
+});
+
+$("#lilscrappy").click(function(){
+	$("#yungjoc").toggle();
+});
+
+
+
+$("#yungjoc").click(function(){
+	$("#gorilla").toggle();
+});
+
+$("#gorilla").click(function(){
+	$("#boyz").toggle();
+});
+
+$("#boyz").click(function(){
+	$("#usda").toggle();
+});
+
+$("#usda").click(function(){
+	$("#pc").toggle();
+});
+
+$("#pc").click(function(){
+	$("#bigkuntry").toggle();
+});
+
+$("#bigkuntry").click(function(){
+	$("#bloodraw").toggle();
+});
+
+$("#bloodraw").click(function(){
+	$("#brisco").toggle();
+});
+
+$("#brisco").click(function(){
+	$("#yung").toggle();
+});
+
+$("#yung").click(function(){
+	$("#slimdunkin").toggle();
+});
+
+$("#slimdunkin").click(function(){
+	$("#wooh").toggle();
+});
+
+$("#wooh").click(function(){
+	$("#frenchie").toggle();
+});
+
+$("#frenchie").click(function(){
+	$("#ojda").toggle();
+});
+
+$("#ojda").click(function(){
+	$("#shawty").toggle();
+});
+
+$("#shawty").click(function(){
+	$("#d4l").toggle();
+});
+
+$("#d4l").click(function(){
+	$("#dem").toggle();
+});
+
+$("#dem").click(function(){
+	$("#crimemob").toggle();
+});
+
+$("#crimemob").click(function(){
+	$("#trillville").toggle();
+});
+
+$("#trillville").click(function(){
+	$("#hurricane").toggle();
+});
+
+$("#hurricane").click(function(){
+	$("#unk").toggle();
+});
+
+$("#unk").click(function(){
+	$("#huey").toggle();
+});
+
+$("#huey").click(function(){
+	$("#jibbs").toggle();
+});
+
+$("#jibbs").click(function(){
+	$("#jkwon").toggle();
+});
+
+$("#jkwon").click(function(){
+	$("#petey").toggle();
+});
+
+
+$("#petey").click(function(){
+	$("#ying").toggle();
+});
+
+$("#ying").click(function(){
+	$("#liljoneast").toggle();
+});
+
+$("#liljoneast").click(function(){
+	$("#eastside").toggle();
+});
+
+$("#eastside").click(function(){
+	$("#liljon").toggle();
+});
+
+$("#liljon").click(function(){
+	$("#david").toggle();
+});
+
+$("#david").click(function(){
+	$("#mike").toggle();
+});
+
+$("#mike").click(function(){
+	$("#slim").toggle();
+});
+
+$("#slim").click(function(){
+	$("#pimp").toggle();
+});
+
+$("#pimp").click(function(){
+	$("#lilkeke").toggle();
+});
+
+$("#lilkeke").click(function(){
+	$("#lilo").toggle();
+});
+
+$("#lilo").click(function(){
+	$("#bigpokey").toggle();
+});
+
+$("#bigpokey").click(function(){
+	$("#hawk").toggle();
+});
+
+$("#hawk").click(function(){
+	$("#fatpat").toggle();
+});
+
+$("#fatpat").click(function(){
+	$("#bigmoe").toggle();
+});
+
+$("#bigmoe").click(function(){
+	$("#djscrew").toggle();
+});
+
+$("#djscrew").click(function(){
+	$("#esg").toggle();
+});
+
+$("#esg").click(function(){
+	$("#bigmello").toggle();
+});
+
+$("#bigmello").click(function(){
+	$("#5thward").toggle();
+});
+
+$("#5thward").click(function(){
+	$("#bushwick").toggle();
+});
+
+$("#bushwick").click(function(){
+	$("#bigmike").toggle();
+});
+
+$("#bigmike").click(function(){
+	$("#mjg").toggle();
+});
+
+$("#mjg").click(function(){
+	$("#8ball").toggle();
+});
+
+$("#8ball").click(function(){
+	$("#8ballmjg").toggle();
+});
+
+$("#8ballmjg").click(function(){
+	$("#tela").toggle();
+});
+
+$("#tela").click(function(){
+	$("#youngbleed").toggle();
+});
+
+$("#youngbleed").click(function(){
+	$("#souljaslim").toggle();
+});
+
+$("#souljaslim").click(function(){
+	$("#silkk").toggle();
+});
+
+$("#silkk").click(function(){
+	$("#cmurder").toggle();
+});
+
+$("#cmurder").click(function(){
+	$("#tru").toggle();
+});
+
+$("#tru").click(function(){
+	$("#miax").toggle();
+});
+
+$("#miax").click(function(){
+	$("#servon").toggle();
+});
+
+$("#servon").click(function(){
+	$("#steady").toggle();
+});
+
+$("#steady").click(function(){
+	$("#biged").toggle();
+});
+
+$("#biged").click(function(){
+	$("#mob").toggle();
+});
+
+$("#mob").click(function(){
+	$("#lilitaly").toggle();
+});
+
+$("#lilitaly").click(function(){
+	$("#lilric").toggle();
+});
+
+$("#lilric").click(function(){
+	$("#seant").toggle();
+});
+
+$("#seant").click(function(){
+	$("#younged").toggle();
+});
+
+$("#younged").click(function(){
+	$("#psycho").toggle();
+});
+
+$("#psycho").click(function(){
+	$("#keylo").toggle();
+});
+
+$("#keylo").click(function(){
+	$("#noh").toggle();
+});
+
+$("#noh").click(function(){
+	$("#cin").toggle();
+});
 // $("img.toggler").click(function(){
 //     $(this).next().toggle(10);
 // });
