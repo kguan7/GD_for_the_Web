77b06d24d6c165d07fbb63d6d858cb603f459944
Karen Guan <KarenGuan@karens-mbp.risd.edu>
2018-05-11 07:53:30 -0400

    3

--- project 3/assets/js/script.js
+++ project 3/assets/js/script.js
@@ -2,35 +2,35 @@ $(document).ready(function(){
 	// your code here
 
 
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
+// function hasGetUserMedia() {
+//   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+// }
 
-if (hasGetUserMedia()) {
-  // Good to go!
-} else {
-  alert('getUserMedia() is not supported by your browser');
-}
+// if (hasGetUserMedia()) {
+//   // Good to go!
+// } else {
+//   alert('getUserMedia() is not supported by your browser');
+// }
 
-const constraints = {
-  video: true,
-  audio: true
+// const constraints = {
+//   video: true,
+//   audio: true
 
-};
+// };
 
 
-const video = document.querySelector('video');
+// const video = document.querySelector('video');
 
-function handleSuccess(stream) {
-  video.srcObject = stream;
-}
+// function handleSuccess(stream) {
+//   video.srcObject = stream;
+// }
 
-function handleError(error) {
-  console.error('Rejected', error);
-}
+// function handleError(error) {
+//   console.error('Rejected', error);
+// }
 
-navigator.mediaDevices.getUserMedia(constraints).
-  then(handleSuccess).catch(handleError);
+// navigator.mediaDevices.getUserMedia(constraints).
+//   then(handleSuccess).catch(handleError);
 
 });
 
