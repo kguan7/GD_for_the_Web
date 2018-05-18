0a9c0074b5d73af44f4bc5550aef2218ea0fcb49
kguan7 <32619797+kguan7@users.noreply.github.com>
2018-05-18 03:20:24 -0400

    Add files via upload

---
+++ project 3/assets/js/script copy.js
@@ -0,0 +1,426 @@
+$(document).ready(function(){
+
+
+  var w1 = $("#w1")
+  var w2 = $("#w2")
+  var w3 = $("#w3")
+  var w4 = $("#w4")
+  var w5 = $("#w5")
+  var w6 = $("#w6")
+
+  var b1 = $("#b1")
+  var b2 = $("#b2")
+  var b3 = $("#b3")
+  var b4 = $("#b4")
+  var b5 = $("#b5")
+  var b6 = $("#b6")
+
+  var d1 = $("#d1")
+  var d2 = $("#d2")
+  var d3 = $("#d3")
+  var d4 = $("#d4")
+  var d5 = $("#d5")
+  var d6 = $("#d6")
+
+  var i1 = $("#i1")
+  var i2 = $("#i2")
+  var i3 = $("#i3")
+  var i4 = $("#i4")
+  var i5 = $("#i5")
+  var i6 = $("#i6")
+
+  var s1 = $("#s1")
+  var s2 = $("#s2")
+  var s3 = $("#s3")
+  var s4 = $("#s4")
+  var s5 = $("#s5")
+  var s6 = $("#s6")
+
+//weird eyes
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 500){
+      $('#w2').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 500){
+      $('#w2').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 1500){
+      $('#w3').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 1500){
+      $('#w3').show();
+    }
+  });
+
+//
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 2500){
+      $('#w4').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 2500){
+      $('#w4').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 3000){
+      $('#w5').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 3000){
+      $('#w5').show();
+    }
+  });
+
+  // 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 5500){
+      $('#w6').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 5500){
+      $('#w6').show();
+    }
+  });
+
+// 
+
+
+// //bubbly eyes
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 2000){
+      $('#b2').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 2000){
+      $('#b2').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 2500){
+      $('#b3').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 2500){
+      $('#b3').show();
+    }
+  });
+
+//
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 3500){
+      $('#b4').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 3500){
+      $('#b4').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 4000){
+      $('#b5').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 4000){
+      $('#b5').show();
+    }
+  });
+
+  // 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 4500){
+      $('#b6').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 4500){
+      $('#b6').show();
+    }
+  });
+
+// 
+
+//demure eyes
+
+ 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 1000){
+      $('#d2').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 1000){
+      $('#d2').show();
+    }
+  });
+
+
+
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 2500){
+      $('#d3').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 2500){
+      $('#d3').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 5000){
+      $('#d4').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 5000){
+      $('#d4').show();
+    }
+  });
+
+  // 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 6000){
+      $('#d5').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 6000){
+      $('#d5').show();
+    }
+  });
+//
+// $(window).scroll(function(){
+//     if($(document).scrollTop() < 6500){
+//       $('#d6').hide();
+//     }
+//   });
+
+//   $(window).scroll(function(){
+//     if($(document).scrollTop() > 6500){
+//       $('#d6').show();
+//     }
+//   });
+// 
+
+// //shocked eyes
+
+ 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 3500){
+      $('#s2').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 3500){
+      $('#s2').show();
+    }
+  });
+
+
+
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 5000){
+      $('#s3').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 5000){
+      $('#s3').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 6500){
+      $('#s4').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 6500){
+      $('#s4').show();
+    }
+  });
+
+  // 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 7000){
+      $('#s5').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 7000){
+      $('#s5').show()
+    }
+  });
+//
+$(window).scroll(function(){
+    if($(document).scrollTop() < 8000){
+      $('#s6').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 8000){
+      $('#s6').show();
+    }
+  });
+
+// intimidating eyes
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 3000){
+      $('#i2').hide();
+    }
+  });
+
+ $(window).scroll(function(){
+    if($(document).scrollTop() > 3000){
+      $('#i2').show();
+    }
+  });
+
+// 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 4000){
+      $('#i3').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 4000){
+      $('#i3').show();
+    }
+  });
+
+  // 
+$(window).scroll(function(){
+    if($(document).scrollTop() < 7500){
+      $('#i4').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 7500){
+      $('#i4').show()
+    }
+  });
+//
+
+$(window).scroll(function(){
+    if($(document).scrollTop() < 8500){
+      $('#i5').hide();
+    }
+  });
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 8500){
+      $('#i5').show();
+    }
+  });
+//
+
+// $(window).scroll(function(){
+//     if($(document).scrollTop() < 9000){
+//       $('#i6').hide();
+//     }
+//   });
+
+  // $(window).scroll(function(){
+  //   if($(document).scrollTop() > 9000){
+  //     $('#i6').show();
+  //   }
+  // });
+
+
+
+
+
+
+//camera stuff
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  // Good to go!
+} else {
+  alert('getUserMedia() is not supported by your browser');
+}
+
+const constraints = {
+  video: true,
+
+
+};
+
+
+const video = document.querySelector('video');
+
+function handleSuccess(stream) {
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Rejected', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+  then(handleSuccess).catch(handleError);
+
+
+
+
+
+
+
+
+
+});
+
