f3a233e3b14fddad68545e856ecf1467f650aa1c
Karen Guan <KarenGuan@karens-mbp.risd.edu>
2018-05-18 03:21:21 -0400

    commit

--- project 3/assets/js/script.js
+++ project 3/assets/js/script.js
@@ -1,36 +1,65 @@
 $(document).ready(function(){
-	// your code here
 
 
-// function hasGetUserMedia() {
-//   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-// }
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
 
-// if (hasGetUserMedia()) {
-//   // Good to go!
-// } else {
-//   alert('getUserMedia() is not supported by your browser');
-// }
+if (hasGetUserMedia()) {
+  // Good to go!
+} else {
+  alert('getUserMedia() is not supported by your browser');
+}
 
-// const constraints = {
-//   video: true,
-//   audio: true
-
-// };
+const constraints = {
+  video: true,
 
 
-// const video = document.querySelector('video');
+};
 
-// function handleSuccess(stream) {
-//   video.srcObject = stream;
-// }
 
-// function handleError(error) {
-//   console.error('Rejected', error);
-// }
+const video = document.querySelector('video');
 
-// navigator.mediaDevices.getUserMedia(constraints).
-//   then(handleSuccess).catch(handleError);
+function handleSuccess(stream) {
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Rejected', error);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).
+  then(handleSuccess).catch(handleError);
+
+
+
+$("#DIV1").hide(); //hide your div initially
+
+var topOfOthDiv1 = $("#DIV2").offset().top;
+var topOfOthDiv3 = $("#DIV3").offset().top;
+
+$(window).scroll(function() {
+    if($(window).scrollTop() < topOfOthDiv1)
+    {
+         $("#DIV1").fadeOut(200); //reached the desired point -- show div
+    }
+    else
+    {
+        if($(window).scrollTop() < topOfOthDiv3) { //scrolled past the other div?
+            $("#DIV1").fadeIn(200); //reached the desired point -- show div
+        }
+        else if($(window).scrollTop() > topOfOthDiv3) { //scrolled past the other div?
+            $("#DIV1").fadeOut(200); //reached the desired point -- show div
+        }
+    }
+
+});
+
+
+
+
+
+
 
 });
 
